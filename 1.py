dae97480d67bb6651b29d6824e0dacc42451621c
shiyi-123-wyb <1173994049@qq.com>
2025-11-25 15:49:51 +0800

    Delete 1.py

--- 1.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('/media/public506/sdb/SY/10k/splits_0.csv')
-print("Total rows:", len(df))
-print("Train samples:", df['train'].notna().sum())
-print("Val samples:", df['val'].notna().sum())
-print("Test samples:", df['test'].notna().sum())
-print("\nSample of the data:")
-print(df.head())
